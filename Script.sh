c9da9efe25fee7aa7a46a8ca2e74ca0173085542
TimofeySamoilov <147207842+TimofeySamoilov@users.noreply.github.com>
2024-10-17 16:33:50 +0300

    Add files via upload

--- Script.sh
+++ Script.sh
@@ -4,6 +4,11 @@ echo -n "Please, give a folder Path:"
 read folder_path
 
 full_path="/mnt$folder_path"
+if [[ -z $folder_path ]]
+then
+  echo "folder path is empty"
+  exit 1
+fi
 if [[ -d $full_path ]]
 then
   echo "This directory was successfully found!"
@@ -18,20 +23,20 @@ then
     fi
     cd $full_path
     old_files_count=3
-    files=$(find ${full_path} -type f -printf '%T@ %p\n' | sort -n | head -n ${old_files_count} | cut -d' ' -f2- | xargs -I {} basename {})
-    sudo tar -czf /home/bevilion/backup/archive.tar.gz --absolute-names --warning=no-file-changed ${files}
-    sudo rm ${files}
+    files=$(find ${full_path} -type f | sort -n | head -n ${old_files_count} | cut -d' ' -f2- | xargs -I {} basename {})
+    tar -czf /home/bevilion/backup/archive.tar.gz --absolute-names --warning=no-file-changed ${files}
+    rm ${files}
     #2point:
     #sudo tar -czf papka.tar.gz . --warning=no-file-changed
     #sudo mv papka.tar.gz /home/bevilion/backup
     if [ $? -eq 0 ]
     then
-      echo "your data was successfully archived and removed!"
+      echo "Your data was successfully archived and removed!"
     else
-      echo "archivation error"
+      echo "Archivation error"
     fi
   else
-    echo "no need to archive"
+    echo "No need to archive"
   fi
 else
   echo "There is no such directory"
--- check.sh
+++ check.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+position="/log"
 #1test:
-sudo fallocate -l 100M /mnt/papka/file1.txt
-sudo fallocate -l 102M /mnt/papka/file2.txt
-sudo fallocate -l 200M /mnt/papka/file3.txt
-./TestScript.sh 1 /papka 40
+sudo fallocate -l 100M /mnt$position/file1.txt
+sudo fallocate -l 102M /mnt$position/file2.txt
+sudo fallocate -l 200M /mnt$position/file3.txt
+./TestScript.sh 1 $position 40
 if [[ -f /home/bevilion/backup/archive.tar.gz ]]
 then
   echo "Test 1 is passed"
 else
   echo "Test 1 is failed"
 fi
+
 #2test:
-sudo fallocate -l 123M /mnt/papka/file1.txt
-sudo fallocate -l 34M /mnt/papka/file2.txt
-sudo touch /mnt/papka/p.py
-sudo touch /mnt/papka/p2.py
-./TestScript.sh 2 /papka 50
+sudo fallocate -l 123M /mnt$position/file1.txt
+sudo fallocate -l 34M /mnt$position/file2.txt
+sudo touch /mnt$position/p.py
+sudo touch /mnt$position/p2.py
+./TestScript.sh 2 $position 50
 if [[ -f /home/bevilion/backup/archive.tar.gz ]]
 then
-  if [[ -f /mnt/papka/p2.py ]]
+  if [[ -f /mnt$position/p2.py ]]
   then
     echo "Test 2 is passed"
   else
@@ -27,31 +29,42 @@ then
 else
   echo "Test 2 is failed"
 fi
-sudo rm /mnt/papka/p2.py
+sudo rm /mnt$position/p2.py
+
 #3test:
-sudo fallocate -l 123M /mnt/papka/file1.txt
-sudo fallocate -l 3M /mnt/papka/file2.txt
-sudo fallocate -l 2M /mnt/papka/file3.txt
-sudo fallocate -l 1M /mnt/papka/file4.txt
-./TestScript.sh 3 /papka 70
-if [[ -f /mnt/papka/file1.txt && -f /mnt/papka/file2.txt && -f /mnt/papka/file3.txt && -f /mnt/papka/file4.txt ]]
+sudo fallocate -l 123M /mnt$position/file1.txt
+sudo fallocate -l 3M /mnt$position/file2.txt
+sudo fallocate -l 2M /mnt$position/file3.txt
+sudo fallocate -l 1M /mnt$position/file4.txt
+./TestScript.sh 3 $position 70
+if [[ -f /mnt$position/file1.txt && -f /mnt$position/file2.txt && -f /mnt$position/file3.txt && -f /mnt$position/file4.txt ]]
 then
   echo "Test 3 is passed"
 else
   echo "Test 3 is failed"
 fi
-sudo rm /mnt/papka/file1.txt /mnt/papka/file2.txt /mnt/papka/file3.txt /mnt/papka/file4.txt
+sudo rm /mnt$position/file1.txt /mnt$position/file2.txt /mnt$position/file3.txt /mnt$position/file4.txt
+
 #4test
-sudo fallocate -l 94M /mnt/papka/file1.txt
-sudo fallocate -l 86M /mnt/papka/file2.txt
-sudo fallocate -l 102M /mnt/papka/file3.txt
-sudo fallocate -l 4M /mnt/papka/file4.txt
-sudo fallocate -l 6M /mnt/papka/file5.txt
-./TestScript.sh 4 /papka 42
-if [[ -f /mnt/papka/file4.txt && -f /mnt/papka/file5.txt ]]
+sudo fallocate -l 94M /mnt$position/file1.txt
+sudo fallocate -l 86M /mnt$position/file2.txt
+sudo fallocate -l 102M /mnt$position/file3.txt
+sudo fallocate -l 4M /mnt$position/file4.txt
+sudo fallocate -l 6M /mnt$position/file5.txt
+./TestScript.sh 4 $position 22
+if [[ -f /mnt$position/file4.txt && -f /mnt$position/file5.txt ]]
 then
   echo "Test 4 is passed"
 else
   echo "Test 4 is failed"
 fi
-sudo rm /mnt/papka/file4.txt /mnt/papka/file5.txt
+sudo rm /mnt$position/file4.txt /mnt$position/file5.txt
+
+#5test
+./TestScript.sh 5 "jskjks" 22
+if [ $? -eq 0 ]
+then
+  echo "Test 5 is passed"
+else
+  echo "Test 5 is failed"
+fi
